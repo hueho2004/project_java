8cb239bbc5fce7d94f01857e4a9e36f89ca937bb
hue <hue.ho25@student.passerellesnumeriques.org>
2023-12-07 13:42:17 +0700

    first commit

---
+++ src/choi/Main.java
@@ -0,0 +1,11 @@
+package choi;
+
+import java.awt.event.KeyEvent;
+
+public class Main {
+	public static void main(String args[])
+	{
+		CarGame c=new CarGame("Car Racing Game");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module choi {
+	requires java.desktop;
+}
